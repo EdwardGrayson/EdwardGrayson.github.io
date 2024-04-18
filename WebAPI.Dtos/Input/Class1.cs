ca03ceaae3f9f99fe338983d566bf36f0927ccc4
Edward Grayson <edwardgrayson@hotmail.com>
2024-04-18 11:55:42 +0100

    Created dto class library

---
+++ WebAPI.Dtos/Input/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Dtos.Input
+{
+    internal class Class1
+    {
+    }
+}
